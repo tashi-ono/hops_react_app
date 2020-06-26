bc7ddbe15da12fbb54cd7d1851986aa16498ba35
Natasha Sana <nfsana@gmail.com>
2020-06-26 07:53:24 -0700

    Updates

---
+++ precache-manifest.9208d0f215ad3c5c88527c0df00cdf9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4935d1916b59790238e1369b3a35cbf9",
+    "url": "/tashi-ono/seir_526_hops_react_app/index.html"
+  },
+  {
+    "revision": "0fc86896f69ac79e2618",
+    "url": "/tashi-ono/seir_526_hops_react_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3434be15cd2181507b75",
+    "url": "/tashi-ono/seir_526_hops_react_app/static/js/2.0ae7b333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tashi-ono/seir_526_hops_react_app/static/js/2.0ae7b333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc86896f69ac79e2618",
+    "url": "/tashi-ono/seir_526_hops_react_app/static/js/main.b715b51f.chunk.js"
+  },
+  {
+    "revision": "f35e97cda4d8056ae01e",
+    "url": "/tashi-ono/seir_526_hops_react_app/static/js/runtime-main.b63eb8a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tashi-ono/seir_526_hops_react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
